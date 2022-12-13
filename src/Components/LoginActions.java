533962d3b5cf3c5d9b9262b66648ecb77b575199
gabrielevil <99329927+gabrielevil@users.noreply.github.com>
2022-12-13 18:45:21 +0200

    added login and register fields

---
+++ src/Components/LoginActions.java
@@ -0,0 +1,9 @@
+package Components;
+
+public class LoginActions {
+    public void onButtonClickHandler(String name) {
+        //System.out.println("button clicked " + name);
+        System.out.println(" ");
+      }
+    
+}
