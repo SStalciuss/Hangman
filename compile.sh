d17f6f33f9b417b1643d77a8d96ea3d93190e85d
Ausrine Zeleckyte <ausrine.zele@gmail.com>
2022-12-19 23:04:44 +0200

    linux scripto uzrasai

--- compile.sh
+++ compile.sh
@@ -3,3 +3,8 @@ javac -d out src/Interfaces/*.java
 javac -d out src/Components/*.java
 javac -d out src/Utils/*.java
 javac -d out src/Main.java
+
+# javac -d out src/Interfaces/*.java
+# javac -d out -cp out src/Components/*.java
+# javac -d out src/Utils/*.java
+# javac -d out -cp out src/Main.java
